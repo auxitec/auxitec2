f677dd256242515098bbc4a0664f431875cd6213
francois.quidbeuf@auxitec.fr <auxitec2018>
2018-03-07 09:49:43 +0100

    Création formulaire inscription

---
+++ includes/frminscription.php
@@ -0,0 +1,27 @@
+<form method="post" action="#">
+    <div>
+        <label form="nom">Nom : </label>
+        <input type=""text" name="nom" />
+    </div>
+
+    <div>
+         <label form="prenom">Prénom : </label>
+        <input type="text" name="prenom" />
+    </div>
+
+    <div>
+        <label form="mail">Adresse mail : </label>
+        <input type="text" name="mail" />
+    </div>
+
+    <div>
+        <label form="mdp">Mot de passe : </label>
+        <input type="password" name="mdp" />
+    </div>
+
+    <div>
+        <input type="reset" value="Effacer" </>
+        <input type="submit" value="Envoyer" </>
+        <input type="hidden" name="frminscription" </>
+    </div>
+</form>
--- index.php
+++ index.php
@@ -1,37 +1,39 @@
-<?php
-include "./includes/header.php";
+<!DOCTYPE html>
+<html lang=""fr-FR">
+    <head>
+        <meta charset=""utf-8" />
+        <title>Auxitec Blog</title>
+        <link rel="stylesheet" href="./assets/css/style.css" />
+    </head>
 
-/*
-echo "<h1>ceci est le corps de mon index.php</h1>";
-
-if (isset($_GET['page']) && $_GET['page'] != "")
-{
-    $page = $_GET['page'];
-}
-else
-{
-    $page = "accueil";
-}
-*/
-
-$page = (isset($_GET['page']) && $_GET['page'] != "") ? $page = $_GET['page'] : "accueil";
-
-$page = "./includes/" . $page . ".inc.php";
+    <body>
+    <div id="container">
 
 
-$files = glob("./includes/*.inc.php");
+        <?php
+            include "./includes/header.php";
 
-if (in_array($page, $files)){
-    include $page;
-}
-else
-    include "./includes/accueil.inc.php";
+            echo "<h1>ceci est le corps de mon index.php</h1>";
 
+            $page = (isset($_GET['page']) && $_GET['page'] != "") ? $page = $_GET['page'] : "accueil";
+            $page = "./includes/" . $page . ".inc.php";
+            $files = glob("./includes/*.inc.php");
 
-/*
-echo "<pre>";
-print_r($files);
-echo $page;
-*/
+            /*
+            //echo "<pre>";
+            //print_r($files);
+            //echo $page;
+            */
 
-include "./includes/footer.php";
+            if (in_array($page, $files)){
+                include $page;
+            }
+            else
+                include "./includes/accueil.inc.php";
+
+            include "./includes/footer.php";
+        ?>
+    </div>
+    </body>
+</html>
+
